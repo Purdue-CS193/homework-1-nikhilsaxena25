abd7ddfb4a56cca8d1e0748ba4afad04a67adbf6
Nikhil Saxena <saxena74@data.cs.purdue.edu>
2021-09-14 23:30:30 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -3,43 +3,59 @@
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
+# Task 1
 
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Homework/MA165/HW2.txt ~/homework-1-nikhilsaxena25/ORIGIN/Homework/MA165/HW1.txt
 
 # Task 2
 
+rm -rf ~/homework-1-nikhilsaxena25/ORIGIN/Memes/SpongeBob/money.jpeg
 
 # Task 3
 
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Memes/'Star Wars'/fafsa.jpg ~/homework-1-nikhilsaxena25/ORIGIN/Memes/SpongeBob
 
 # Task 4
 
+rm -rf ~/homework-1-nikhilsaxena25/ORIGIN/Homework/CS191
 
 # Task 5
 
+cd ~/homework-1-nikhilsaxena25/ORIGIN/Programs
+mkdir C++
 
 # Task 6
 
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Age.cpp ~/homework-1-nikhilsaxena25/ORIGIN/Programs/C++
 
 # Task 7
 
+cp -r ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Java ~/homework-1-nikhilsaxena25/ORIGIN/Homework/CS180
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Homework/CS180/Java ~/homework-1-nikhilsaxena25/ORIGIN/Homework/CS180/Project_1
 
 # Task 8
 
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Count.c ~/homework-1-nikhilsaxena25/ORIGIN/Programs/C
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Goodbye.c ~/homework-1-nikhilsaxena25/ORIGIN/Programs/C
 
 # Task 9
 
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Name.py ~/homework-1-nikhilsaxena25/ORIGIN/Programs/'Python 3'
+mv ~/homework-1-nikhilsaxena25/ORIGIN/Programs/Space.py ~/homework-1-nikhilsaxena25/ORIGIN/Programs/'Python 3'
 
 # Task 10
 
+cd ~/homework-1-nikhilsaxena25/ORIGIN/Homework/POL141
+mkdir Essay.txt
 
 # Task 11
 
+ls ..
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
-
-
+set number
+syntax on
+set shiftwidth=2 softtabstop=2 tabstop=2
 '
